7eabf1af0f1558dd536804db678016989d567495
matthieu.comby <matthieu.comby@fullsix.net>
2017-06-30 15:02:16 +0200

    feat(stepDef): improve step for getting root object

--- tests/helper.test.js
+++ tests/helper.test.js
@@ -36,3 +36,49 @@ test('should allow to count object properties', () => {
         })
     ).toBe(5)
 })
+
+test('should allow to count nested objects properties', () => {
+    expect(
+        Helper.countNestedProperties({
+            a: true,
+            b: true,
+            c: {
+                d: 'value1',
+                e: 'value2'
+            }
+        })
+    ).toBe(4)
+})
+
+test('should allow to count object properties with null, undefined properties ', () => {
+    expect(
+        Helper.countNestedProperties({
+            a: null,
+            b: undefined,
+            c: 'value3'
+        })
+    ).toBe(3)
+})
+
+test('should allow to count object with properties array property', () => {
+    expect(
+        Helper.countNestedProperties({
+            a: [1, 2],
+            b: true,
+            c: true
+        })
+    ).toBe(4)
+})
+
+test('should allow to count object properties with empty array property', () => {
+    expect(
+        Helper.countNestedProperties({
+            a: true,
+            b: true,
+            c: {
+                d: '',
+                e: []
+            }
+        })
+    ).toBe(4)
+})
