4847a4656747c412feb9e98d4ded1380e4b27fda
Raphaël Benitte <raphael.benitte@fullsix.com>
2017-07-19 12:55:20 +0900

    test(file-system): add tests on file system extension

--- tests/__mocks__/chai.js
+++ tests/__mocks__/chai.js
@@ -9,11 +9,13 @@ const chai = jest.genMockFromModule('chai')
 const equal = jest.fn()
 const contain = jest.fn()
 const match = jest.fn()
+const fail = jest.fn()
 
 const chainable = {
     equal,
     contain,
-    match
+    match,
+    fail
 }
 
 chainable.to = chainable
@@ -23,15 +25,18 @@ chainable.empty = chainable
 chainable.deep = chainable
 
 const expect = jest.fn(() => chainable)
+expect.fail = fail
 
 exports.expect = expect
 exports.equal = equal
 exports.contain = contain
 exports.match = match
+exports.fail = fail
 
 exports.clear = () => {
     expect.mockClear()
     equal.mockClear()
     contain.mockClear()
     match.mockClear()
+    fail.mockClear()
 }
