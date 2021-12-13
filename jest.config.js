51156b36fa93f180491164ad2c9ad1a3fa834ad8
tienduy-nguyen <tienduy.nguyen.dev@gmail.com>
2021-12-13 13:35:38 +0100

    feat(ts): init project with typescript

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,11 @@
 module.exports = {
-    coveragePathIgnorePatterns: ['/node_modules/', '/tests/'],
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    clearMocks: true,
+    moduleFileExtensions: ['js', 'ts'],
+    testRegex: '.*\\.test\\.(t|j)s$',
+    transform: {
+        '^.+\\.(t|j)s$': 'ts-jest',
+    },
+    coverageDirectory: './coverage',
 }
