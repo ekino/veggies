f135c81e3ada96bbb5264a15171d2e4ed3e9f625
tduyng <tduyng.dev@gmail.com>
2025-02-07 17:51:46 +0100

    chore: remove arg, use node:util, clean bin command

--- bin/veggies.cjs
+++ bin/veggies.cjs
@@ -1,10 +1,14 @@
-'use strict'
+#!/usr/bin/env node
 
-import arg from 'arg'
-import { createRequire } from 'module'
+const { parseArgs } = require('node:util')
+const CucumberCli = require('@cucumber/cucumber/lib/cli/index').default
 
-const CucumberCli = createRequire(import.meta.url)('@cucumber/cucumber/lib/cli/index').default
-
+const veggiesArgsOptions = {
+    cleanSnapshots: { type: 'boolean' },
+    updateSnapshots: { type: 'boolean', short: 'u' },
+    preventSnapshotsCreation: { type: 'boolean' },
+    help: { type: 'boolean', short: 'h' },
+}
 const veggiesArgsDefinitions = {
     '--cleanSnapshots': Boolean,
     '--updateSnapshots': Boolean,
@@ -21,29 +25,33 @@ Options:
   --preventSnapshotsCreation  a snapshot related step that would create one will fail instead (useful on CI environment)
   
 For more details please visit https://github.com/ekino/veggies/blob/master/README.md
-    `)
+  `)
     console.log('cucumber-js help\n')
 }
 
-export const run = async (argv) => {
-    const { _: cucumberArgs } = arg(veggiesArgsDefinitions, { argv, permissive: true })
-
+const run = async (argv) => {
     try {
-        if (cucumberArgs.includes('--help')) {
-            printHelp()
-        }
+        const { values } = parseArgs({
+            args: argv.slice(2),
+            options: veggiesArgsOptions,
+            strict: false,
+            allowPositionals: true,
+        })
+
+        if (values.help) printHelp()
 
         const result = await new CucumberCli({
-            argv: cucumberArgs,
+            argv,
             cwd: process.cwd(),
             stdout: process.stdout,
             stderr: process.stderr,
             env: process.env,
         }).run()
-
         process.exit(result.success ? 0 : 1)
     } catch (error) {
         console.error(error)
         process.exit(1)
     }
 }
+
+run(process.argv)
--- bin/veggies.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-import { run } from '../src/cli/index.js'
-
-run(process.argv)
